10a3c8f85efccf4a1a9d2eb9799db727aa739928
Brian Kershaw <x13127659@student.ncirl.ie>
2018-12-06 19:08:46 +0000

    Add Products Controller & View

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'pages/about'
+  
+  # namespace for admin routes
+  namespace :admin do
+    resources :products
+    # resources :orders
+    # resources :users
+    # resources :product
+  end
 end
