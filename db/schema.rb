e9919adcdf145644acb3d1e6123c83a94e4a4738
Brian Kershaw <x13127659@student.ncirl.ie>
2018-12-06 18:10:03 +0000

    Add Seeders and alter migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_04_171152) do
+ActiveRecord::Schema.define(version: 2018_12_05_175934) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name", null: false
@@ -39,6 +39,8 @@ ActiveRecord::Schema.define(version: 2018_12_04_171152) do
     t.string "status", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
   create_table "product_categories", force: :cascade do |t|
