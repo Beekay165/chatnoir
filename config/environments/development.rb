a11fa1d3c9f296fef46a82e5232bbc871d789e1c
GabiAntohi <gabrielaantohi.eu@gmail.com>
2018-12-23 15:10:37 +0000

    precompiling ES6 uglifier issue

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -64,7 +64,7 @@ Rails.application.configure do
   config.action_controller.include_all_helpers = false
 
  
-  config.action_mailer.default_url_options = { :host => 'localhost:5432' }
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {:address => "localhost", :port => 1025}
 end
